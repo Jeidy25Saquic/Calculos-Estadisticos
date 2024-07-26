2d13e120b397758548e2a9f451a9eb9a32659757
Jennifer Saquic <jenntecum2001@gmail.com>
2024-07-25 23:11:52 -0600

    Calculos en  clase calculadora

---
+++ Estadistica/Estadistica/Calculadora.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estadistica
+{
+    public class Calculadora
+    {
+
+        public double CalcularMedia(List<double> numeros)
+        {
+
+            double suma = 0;
+            foreach (var numero in numeros)
+            {
+                suma = numero + suma;
+            }
+
+            double Media = suma / numeros.Count;
+            return Media;
+        }
+
+        public double CalcularMediana(List<double> numeros)
+        {
+            // Ordena la lista de números de menor a mayor
+            numeros.Sort();
+
+            int cantidadNumeros = numeros.Count;
+            double mediana;
+
+            if (cantidadNumeros % 2 == 0)
+            {
+                // Si la cantidad de números es par
+                int indiceMitad1 = cantidadNumeros / (2 - 1);
+                int indiceMitad2 = cantidadNumeros / 2;
+                mediana = (numeros[indiceMitad1] + numeros[indiceMitad2]) / 2;
+            }
+            else
+            {
+                // Si la cantidad de números es impar
+                int indiceCentral = cantidadNumeros / 2;
+                mediana = numeros[indiceCentral];
+            }
+
+            return mediana;
+
+
+        }
+
+        public double CalcularDesviacionEstandar(List<double> numeros)
+        {
+            /*Paso 1: calcular la media.
+          Paso 2: calcular el cuadrado de la distancia a la media para cada dato.
+          Paso 3: sumar los valores que resultaron del paso 2.
+          Paso 4: dividir entre el número de datos.
+          Paso 5: sacar la raíz cuadrada.*/
+            double sumaDiferenciasCuadrados = 0;
+            foreach (double numero in numeros)
+            {
+                double diferencia = numero - CalcularMedia(numeros);
+           sumaDiferenciasCuadrados = sumaDiferenciasCuadrados+(diferencia * diferencia);
+            }
+
+            // Calcula la desviación estándar
+            double DesviacionEstandar = Math.Sqrt(sumaDiferenciasCuadrados / (numeros.Count - 1)); // (n-1)
+            return DesviacionEstandar;
+
+        }
+
+    }
+}
---
+++ Estadistica/Estadistica/Program.cs
@@ -0,0 +1,83 @@
+﻿using Estadistica;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+namespace Banco;
+class Program
+{
+
+    static void Main(string[] args)
+    {  
+        bool continuar=true;
+        Calculadora calculadora = new Calculadora();
+        Numeros numeracion = new Numeros();
+        List<double> listaNumeros = Numeros.numeros;
+        do
+        {
+           
+            try
+            {
+                
+                listaNumeros.Clear();
+                Console.WriteLine("\nMenú:");
+                Console.WriteLine("1. Calcular media");
+                Console.WriteLine("2. Calcular mediana");
+                Console.WriteLine("3. Calcular moda");
+                Console.WriteLine("4. Calcular desviación estándar");
+                Console.WriteLine("5. Salir");
+                Console.Write("Selecciona una opción: ");
+
+                int  opcion = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+
+                Console.WriteLine("\n");
+                switch (opcion)
+                {
+                    case 1:
+                       
+                        Console.WriteLine("------------------------->Media<-------------------");
+                        numeracion.CantidadDeNumeros();
+                        Console.WriteLine("El resultado de la media es: " + calculadora.CalcularMedia(listaNumeros));
+
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                    case 2:
+                        Console.WriteLine("------------------------->Mediana<-------------------");
+                        numeracion.CantidadDeNumeros();
+                        Console.WriteLine("El resultado de la mediana es: " + calculadora.CalcularMediana(listaNumeros));
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                    case 3:
+                        Console.WriteLine("Moda");
+                        break;
+                    case 4:
+                        Console.WriteLine("------------------------->Mediana<-------------------");
+                        numeracion.CantidadDeNumeros();
+                        Console.WriteLine("El resultado de la desviasion estandar es: " + calculadora.CalcularDesviacionEstandar(listaNumeros));
+                        Console.ReadKey();
+                        Console.Clear();
+                        break;
+                    case 5:
+                        Console.WriteLine(" Ha salido, vuelva pronto...");
+                        continuar = false;
+                        break;
+                    default:
+                        Console.WriteLine("Opción no válida. Inténtalo de nuevo.");
+                      
+                        break;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Ha ocurrido un error,intentalo de nuevo...");
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+                Console.Clear();
+            }
+
+        } while(continuar==true);
+       
+       
+    }
+}
